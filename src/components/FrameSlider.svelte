b5c42fe47f88610efd8724a8769b7d1098628d71
Hanson <105476@grundfos.com>
2024-05-17 15:29:15 +0800

    1

--- src/components/FrameSlider.svelte
+++ src/components/FrameSlider.svelte
@@ -82,32 +82,29 @@
 </script>
 
 <div class="slidecontainer">
-	<div class="scales">
-		<div class="scale-panel">
-			<div>
-				<button
-					on:click={() => {
-						dispatch("addKeyframe", { frame_idx: current_value });
-					}}
-					disabled={!_selectedBone}
-				>
-					<span>Add Keyframe</span>
-				</button>
-				<button
-					on:click={() => {
-						dispatch("deleteKeyframe", {
-							frame_idx: deleteFrameIdx,
-						});
-						// do this in a callback is more proper, settle for now
-						deleteFrameIdx = null;
-					}}
-					disabled={deleteFrameIdx === null}
-				>
-					<span>Delete Keyframe</span>
-				</button>
-			</div>
+	<div class="keyframe-panel">
+		<div>
+			<button
+				on:click={() => {
+					dispatch("addKeyframe", { frame_idx: current_value });
+				}}
+				disabled={!_selectedBone}
+			>
+				<span>Add Keyframe</span>
+			</button>
+			<button
+				on:click={() => {
+					dispatch("deleteKeyframe", {
+						frame_idx: deleteFrameIdx,
+					});
+					// do this in a callback is more proper, settle for now
+					deleteFrameIdx = null;
+				}}
+				disabled={deleteFrameIdx === null}
+			>
+				<span>Delete Keyframe</span>
+			</button>
 		</div>
-		<div></div>
 	</div>
 	<div bind:this={slider}></div>
 	{#each keyFrames as value, idx (value)}
@@ -129,53 +126,9 @@
 		width: 100%; /* Width of the outside container */
 	}
 
-	/* The slider itself */
-	.slider {
-		-webkit-appearance: none; /* Override default CSS styles */
-		appearance: none;
-		width: 100%; /* Full-width */
-		height: 30px; /* Specified height */
-		background: #d3d3d3; /* Grey background */
-		outline: none; /* Remove outline */
-		opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
-		-webkit-transition: 0.2s; /* 0.2 seconds transition on hover */
-		transition: opacity 0.2s;
-		border: 0;
-		padding: 0;
-		margin: 0;
-	}
-
-	/* Mouse-over effects */
-	.slider:hover {
-		opacity: 1; /* Fully shown on mouse-over */
-	}
-
-	/* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
-	.slider::-webkit-slider-thumb {
-		-webkit-appearance: none; /* Override default look */
-		appearance: none;
-		width: 10px; /* Set a specific slider handle width */
-		height: 30px; /* Slider handle height */
-		background: #04aa6d; /* Green background */
-		cursor: pointer; /* Cursor on hover */
-	}
-
-	.slider::-moz-range-thumb {
-		width: 25px; /* Set a specific slider handle width */
-		height: 25px; /* Slider handle height */
-		background: #04aa6d; /* Green background */
-		cursor: pointer; /* Cursor on hover */
-	}
-
-	.scales {
-		width: 100%;
-		height: 30px;
-		text-align: left;
-	}
-
-	.scales .scale-panel {
+	.keyframe-panel {
 		display: flex;
-		justify-content: space-between;
+		justify-content: flex-end;
 		align-items: center;
 	}
 
