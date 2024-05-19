762415abc17955259e7499166904b369ba08870e
hansen1416 <badapplesweetie@gmail.com>
2024-05-19 13:15:37 +0800

    1

--- src/routes/Recorder.svelte
+++ src/routes/Recorder.svelte
@@ -26,12 +26,49 @@
 </script>
 
 <section>
-	<input type="file" accept="video/*" on:change={uploadVideo} />
-
-	<video controls bind:this={video}>
-		<track kind="captions" srclang="en" label="English" default />
-	</video>
+	<div class="left-hand">
+		<div class="input-box">
+			<input type="file" accept="video/*" on:change={uploadVideo} />
+		</div>
+		<div class="video-box">
+			<video controls={true} bind:this={video}>
+				<track kind="captions" srclang="en" label="English" default />
+			</video>
+		</div>
+	</div>
+	<div class="right-hand"></div>
 </section>
 
 <style>
+	section {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.left-hand,
+	.right-hand {
+		flex-grow: 1;
+		flex-basis: 50%;
+	}
+
+	.left-hand {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+	}
+
+	.input-box {
+		margin: 10px 0;
+	}
+
+	.video-box {
+		max-width: 80%;
+		max-height: 80%;
+	}
+
+	video {
+		max-width: 100%;
+		max-height: 100%;
+	}
 </style>
